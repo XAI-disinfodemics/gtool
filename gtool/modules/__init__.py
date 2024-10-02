c7071357b000a102e240ba89813efc477c0621dd
alfonso-hp <alfonsodepazg@gmail.com>
2024-10-02 22:31:24 +0200

    fix: Use  instead of importer from pkgutil (deprecated)

--- gtool/modules/__init__.py
+++ gtool/modules/__init__.py
@@ -1,20 +1,20 @@
 import pkgutil
-
+import importlib
 
 __all__ = []
 
-
 def _import_modules():
-	""" Import all modules automatically (that allows things like BaseEngine.__subclasses__()
+    """Import all modules automatically (to allow things like BaseEngine.__subclasses__()
     without explicitly importing each subclass)
     """
-	prefixLen = len(__name__) + 1
-	for importer, moduleName, isPkg in pkgutil.iter_modules(__path__, prefix = f'{__name__}.'):
-		assert not isPkg
-		moduleNameWithoutPrefix = moduleName[prefixLen:]
-		__all__.append(moduleNameWithoutPrefix)
-		module = importer.find_module(moduleName).load_module(moduleName)
-		globals()[moduleNameWithoutPrefix] = module
+    prefix_len = len(__name__) + 1
+    for _, module_name, is_pkg in pkgutil.iter_modules(__path__, prefix=f'{__name__}.'):
+        assert not is_pkg
+        module_name_without_prefix = module_name[prefix_len:]
+        __all__.append(module_name_without_prefix)
 
+        # Use importlib to import the module
+        module = importlib.import_module(module_name)
+        globals()[module_name_without_prefix] = module
 
 _import_modules()
